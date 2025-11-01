a6dfa5cf93fc9ebb7f6fd032b1c1dc3fc9c3c885
Junaid8217 <junaidhossain77349@gmail.com>
2025-11-01 17:19:13 +0600

    Created user context for shared authentication

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Root from './layout/Root.jsx';
 import Home from './components/Home/Home.jsx';
 import Login from './components/Login/Login.jsx';
 import Register from './components/Register/Register.jsx';
+import AuthProvider from './Context/AuthContext/AuthProvider.jsx';
 
 
 
@@ -36,7 +37,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     
   </StrictMode>,
 )
